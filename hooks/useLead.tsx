d232a10d6ce0b2298aec58ef878a2ff2e8d6bd59
Hai Tran <tranngochai171@gmail.com>
2023-02-07 22:43:26 +0700

    chore: show successfull message when submit enquiry

--- hooks/useLead.tsx
+++ hooks/useLead.tsx
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import axiosHelper from 'helpers/axios.helper';
+import { toast } from 'react-toastify';
 
 type SubmitEnquiryPayload = {
   name: string;
@@ -12,5 +13,8 @@ export const useSubmitEnquiry = () => {
   return useMutation({
     mutationFn: (payload: SubmitEnquiryPayload) =>
       axiosHelper.post('/api/leads', payload),
+    onSuccess: () => {
+      toast.success('Successfully Submitted Enquiry');
+    },
   });
 };
